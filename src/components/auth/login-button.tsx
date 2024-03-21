f9178ddb33d54ddb79fc1e97b72285f4e1f9f1e9
Igor Fidler <igorgfidler@hotmail.com>
2024-03-21 10:41:23 -0300

    wrapper

---
+++ src/components/auth/login-button.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+type LoginButtonProps = {
+  children: React.ReactNode;
+  mode?: "modal" | "redirect";
+  asChild?: boolean;
+};
+
+export function LoginButton({
+  children,
+  asChild,
+  mode = "redirect",
+}: LoginButtonProps) {
+  const router = useRouter();
+  const onClick = () => {
+    router.push("/auth/login");
+  };
+
+  if (mode === "modal") {
+    return <span>TODO implemente modal</span>;
+  }
+  return (
+    <span onClick={onClick} className="cursor-pointer">
+      {children}
+    </span>
+  );
+}
---
+++ src/components/auth/login-form.tsx
@@ -0,0 +1,13 @@
+import { CardWrapper } from "./card-wrapper";
+
+export function LoginForm() {
+  return (
+    <CardWrapper
+      headerLabel="Welcome Back"
+      backButtonLabel="Don't have an account?"
+      backButtonHref="/auth/register"
+    >
+      Loggin Form
+    </CardWrapper>
+  );
+}
